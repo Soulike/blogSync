e6c91ebb1aae6c2a6cd4a753f24b5b510dd5c10b
Soulike <316413310@qq.com>
2018-04-19 16:06:38 +0800

    修改目录

--- functions/git.js
+++ functions/git.js
@@ -2,7 +2,7 @@
 const asyncFunctions = require('./asyncFunctions');
 const {log} = require('./log');
 
-const PATH = '/home/soulike/blog/source';
+const PATH = '~/blog/source';
 
 async function clone(syncPage)
 {
